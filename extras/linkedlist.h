3a5a1b0aa01b7048d042dc562b6dd7992e5016f8
skulluglify <skulluglify@outlook.com>
2021-09-28 21:19:41 +0700

    noop

---
+++ extras/linkedlist.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _LINKEDLIST_H_
+#define _LINKEDLIST_H_
+
+#ifdef __cplusplus
+
+typedef struct {
+	short id;
+	int *before;
+	long int value;
+	int *after;
+} MemberList;
+
+// add
+// remove
+// set
+// index
+// get
+// begin
+// end
+
+#endif
+
+#endif /*_LINKEDLIST_H_*/
