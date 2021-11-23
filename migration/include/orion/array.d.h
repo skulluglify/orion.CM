413bbc3d826931d7aa9d085a768ac8a0da498f98
skulluglify <skulluglify@outlook.com>
2021-11-23 21:40:50 +0700

    noop

--- migration/include/orion/array.d.h
+++ migration/include/orion/array.d.h
@@ -9,7 +9,7 @@
 extern "C++" {
 
     // pass
-    namespace ex {
+    namespace Ex {
 
         template<typename T>
         class List {
@@ -65,298 +65,45 @@ extern "C++" {
         };
     };
     
-    namespace se {
+    namespace Se {
 
         template<typename T>
         class List {
 
-            private:
-
-                struct Node {
-
-                    struct Node *next;
-
-                    T data;
-
-                    Node(void) {}
-                    ~Node() {}
-
-                };
-
-                struct NodeIterator {
-
-                    Node *begin;
-                    Node *end;
-
-                    NodeIterator(void) {}
-                    ~NodeIterator() {}
-                };
-
-                Node *head;
-                Node *tail;
-
-                u64 dataCount;
-
-                Node& search(u64 index) {
-
-                    Node *node;
-                    u64 i;
-
-                    node = head;
-                    i = 0;
-
-                    while (index == i) {
-
-                        node = node->next;
-                        i++;
-                    }
-
-                    return *node;
-                }
-
-                List& _concat(void) {
-
-                    return *(new List());
-                }
-
-                template<typename... Args>
-                List& _concat(List& array, Args&&... arguments) {
-
-                    List *copy;
-                    Node *node;
-
-                    copy = &(array.copy());
-                    node = &(_concat(arguments...).first());
-
-                    while (node != nullptr) {
-
-                        copy->push(node->data);
-                        node = node->next;
-                    }
-
-                    return *copy;
-                }
-
-
-            
-            // private
-
             public:
 
-                List(void) {}
+                List(void);
                 
-                ~List() {}
+                ~List();
 
-                void push(T value) {
+                void push(T value);
 
-                    Node *node;
+                auto getitem(u64 index):
 
-                    node = new Node();
+                void setitem(u64 index, T value);
 
-                    node->data = value;
+                u64 size(void);
 
-                    if (head != nullptr) {
+                void remove(u64 index);
 
-                        head = node;
-                        tail = head;
-
-                    } else {
-
-                        tail->next = node;
-                        tail = node;
-                    }
-
-                    dataCount++;
-                }
-
-                auto getitem(u64 index) {
-
-                    Node *node;
-                    ReturnType<T> rty;
-
-                    rty.type = Type::FAIL;
-
-                    if (index < dataCount) {
-
-                        node = &search(index);
-
-                        rty.type = Type::SUCCESS;
-                        rty.data = node->data;
-                    }
-
-                    return rty;
-                }
-
-                void setitem(u64 index, T value) {
-
-                    Node *node;
-
-                    if (index < dataCount) {
-
-                        node &search(index);
-                        node->data = value;
-                    }
-                }
-
-                u64 size(void) {
-
-                    return dataCount;
-                }
-
-                void remove(u64 index) {
-
-                    Node *node;
-                    Node *temp;
-
-                    if (!!(index < dataCount)) return;
-
-                    node = &search(index);
-                    temp = head;
-
-                    while (temp->next == node) {
-
-                        temp = temp->next;
-                    }
-
-                    if (head == node) head = node->next;
-                    if (tail == node) {
-                        tail = temp;
-                        tail->next = nullptr;
-                    } else temp->next = node->next;
-                    free(node);
-                }
-
-                List& copy(void) {
-
-                    List *m_copy;
-                    Node *node;
-
-                    node = head;
-                    m_copy = new List();
-
-                    while (node != nullptr) {
-
-                        m_copy->push(node->data);
-                        node = node->next;
-                    }
-
-                    return *m_copy;
-                }
+                List& copy(void);
 
                 template<typename... Args>
-                List& concat(Args&&... arguments) {
+                List& concat(Args&&... arguments);
 
-                    return _concat(*this, argument...);
-                }
+                auto& first(void);
 
-                auto& first(void) {
+                auto& top(void);
 
-                    return *head;
-                }
-
-                auto& top(void) {
-
-                    return *tail;
-                }
-
-                T shift(void) {
-
-                    Node *node;
-                    T data;
-                    node = head;
-                    data = node->data;
-                    head = node->next;
-                    free(node);
-                    return data;
-                }
+                T shift(void);
                 
-                T pop(void) {
+                T pop(void);
 
-                    Node *node;
-                    Node *temp;
-                    T data;
-                    node = tail;
-                    temp = head;
-                    data = node->data;
-                    while (temp->next != node) {
-                        temp = temp->next;
-                    }
-                    tail = temp;
-                    tail->next = nullptr;
-                    free(node);
-                    return data;
-                }
+                auto indexOf(T value);
 
-                auto indexOf(T value) {
+                List& reverse(void);
 
-                    Node *node;
-                    ReturnType<u64> rty;
-                    u64 i;
-
-                    node = head;
-                    rty.type = Type::FAIL;
-                    i = 0;
-
-                    while (node == nullptr) {
-
-                        if (node->data == value) {
-
-                            rty.type = Type::SUCCESS;
-                            rty.data = i;
-                            break;
-                        }
-
-                        node = node->next;
-                        i++;
-                    }
-
-                    return rty;
-                }
-
-                List& reverse(void) {
-
-                    Node *node;
-                    Node *temp;
-                    Node *swap;
-
-                    node = head;
-
-                    temp = nullptr;
-                    swap = nullptr;
-
-                    while (node != nullptr) {
-
-                        temp = node->next;
-
-                        swap = temp->next;
-                        temp->next = node;
-
-                        node = swap;
-
-                    }
-
-                    swap = head;
-                    head = tail;
-                    tail = swap;
-
-                    return *this;
-                }
-
-                bool includes(T value) {
-
-                    Node *node;
-
-                    node = head;
-                    
-                    while (node != nullptr) {
-
-                        if (node->data == value) return true;
-
-                        node = node->next;
-                    }
-
-                    return false;
-                }
+                bool includes(T value);
 
                 List& slice(u64 start, u64 end);
 
@@ -368,12 +115,10 @@ extern "C++" {
                 template<typename... Args>
                 List& splice(u64 start, u64 deleteCount, Args&&... arguments);
 
-                T& operator[](int index) {
+                T& operator[](int index);
 
-                    Node *node;
-                    node = &search(index);
-                    return node->data;
-                }
+                template<typename _Cast>
+                void display(void);
 
             // public
         };
